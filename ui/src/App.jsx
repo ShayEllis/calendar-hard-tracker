ddb3e910ebb6ff7015ad16465ae4ef0d2cd8cd57
ShayEllis <58398990+ShayEllis@users.noreply.github.com>
2024-04-12 15:37:20 -0700

    started working on loading screen when calenadar is first opened

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,4 +1,4 @@
-import { useReducer, useEffect } from 'react'
+import { useReducer, useEffect, useState } from 'react'
 import { Calendar } from './components/calendar/calendar'
 import './App.css'
 import { reducer, initialState } from './reducers/appReducer'
@@ -7,10 +7,13 @@ import {
   CalendarDispatchContext,
 } from './context/calendarContexts'
 import { calendarServer } from './utils/calendarServer'
+import Backdrop from '@mui/material/Backdrop'
+import CircularProgress from '@mui/material/CircularProgress'
 
 function App() {
   // Manages all calendar state
   const [state, dispatch] = useReducer(reducer, initialState)
+  const [fetchingData, setFetchingData] = useState(true)
 
   useEffect(() => {
     calendarServer
@@ -18,6 +21,8 @@ function App() {
       .then((response) => {
         if (response)
           dispatch({ type: 'app/loadCalenderDayData', payload: response })
+          setFetchingData(false)
+          
       })
       .catch((e) => {
         console.error(`Failed to update calendar state - ${e.message}`)
@@ -27,6 +32,15 @@ function App() {
   return (
     <CalendarContext.Provider value={state}>
       <CalendarDispatchContext.Provider value={dispatch}>
+        <Backdrop
+          sx={{
+            color: 'rgb(120, 85, 137)',
+            zIndex: (theme) => theme.zIndex.drawer + 1,
+          }}
+          open={fetchingData}
+          onClick={() => console.log('clicked')}>
+          <CircularProgress color='inherit' />
+        </Backdrop>
         <Calendar />
       </CalendarDispatchContext.Provider>
     </CalendarContext.Provider>
